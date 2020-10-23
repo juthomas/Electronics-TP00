64806c1fa1448b03e37bbb4560067a30275fdda3
labelec <labelec@e1r1p5.42.fr>
2020-10-23 14:08:08 +0200

    [ADD] Ez

--- ex04/main.c
+++ ex04/main.c
@@ -3,62 +3,37 @@
 #define PIN2 2
 #define CPU_CLOCK 2000000
 
-
-void	wait_x_cpu_clocks2(int cpu_clocks)
-{
-	while (cpu_clocks > 0)
-	{
-
-		// NOP();
-		cpu_clocks-=1;
-		//wait_x_cpu_clocks(10000);
-	}
-}
-
 void	wait_x_cpu_clocks(int32_t cpu_clocks)
 {
 	while (cpu_clocks > 0)
 	{
-
-		// NOP();
 		cpu_clocks-=3;
-		// wait_x_cpu_clocks2(10000);
 	}
 }
-
 
 void	custom_delay(int milli)
 {
 	//milli = 0,001s
-	milli = milli *	16000;
-	wait_x_cpu_clocks(milli - 2);
+	milli = milli *	2000;
+	wait_x_cpu_clocks(milli - 5);
 }
 
 int		main()
 {
 	DDRB |= (1 << PIN3);
-	// DDRD &= (0 << PIN3);
 	DDRD = 0b00000000;
-
-	// DDRB |= (1 << PIN2);
-	PORTB &= (0 << PIN3);
-	// PORTB |= (1 << PIN2);
-
-	// PORTD = 0b00000000;
+	PORTB = 0b00000000;
 
 	for(;;)
 	{
 		if (!(PIND & (1 << PIN3)))
 		{
-			// PORTB = 0b00000000;
 			PORTB |= (1 << PIN3);
 		}
 		else
 		{
-			PORTB &= (0 << PIN3);
-
+			PORTB &= ~(1 << PIN3);
 		}
-		
 	}
 	return (0);
 }
--- ex06/main.c
+++ ex06/main.c
@@ -1,6 +1,8 @@
 #include <avr/io.h>
 #define PIN3 3
 #define PIN2 2
+#define PIN1 1
+#define PIN0 0
 #define CPU_CLOCK 2000000
 
 void	wait_x_cpu_clocks(int32_t cpu_clocks)
@@ -21,19 +23,20 @@ void	custom_delay(int milli)
 int		main()
 {
 	DDRB |= (1 << PIN3);
-	//Reverse led
-	// DDRB |= (1 << PIN2);
+	DDRB |= (1 << PIN2);
+	DDRB |= (1 << PIN1);
+	DDRB |= (1 << PIN0);
+
 	DDRD = 0b00000000;
 
+	PORTB = 0b00000000;
 
 	for(;;)
 	{
-		if ((PIND & (1 << PIN3)))
+		if (!(PIND & (1 << PIN3)))
 		{
-			//Reverse led
-			// PORTB ^= (1 << PIN2);
-			while ((PIND & (1 << PIN3)));
-			PORTB ^= (1 << PIN3);
+			while (!(PIND & (1 << PIN3)));
+			PORTB = (PORTB & 0b00001111) == 0b00001111 ? PORTB & (0b11110000) : PORTB + 1;
 			custom_delay(200);
 		}
 		
