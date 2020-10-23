beb31576e11f472cb0bfdfacf9271803d3414a5b
labelec <labelec@e1r1p5.42.fr>
2020-10-23 11:47:34 +0200

    [ADD] working

--- ex03/main.c
+++ ex03/main.c
@@ -1,12 +1,31 @@
 #include <avr/io.h>
 #define PIN3 3
+#define PIN2 2
 #define CPU_CLOCK 16000000
 
-void	wait_x_cpu_clocks(int cpu_clocks)
+
+void	wait_x_cpu_clocks2(int cpu_clocks)
 {
-	while (cpu_clocks)
-		cpu_clocks-=2;
+	while (cpu_clocks > 0)
+	{
+
+		// NOP();
+		cpu_clocks-=1;
+		//wait_x_cpu_clocks(10000);
+	}
 }
+
+void	wait_x_cpu_clocks(int32_t cpu_clocks)
+{
+	while (cpu_clocks > 0)
+	{
+
+		// NOP();
+		cpu_clocks-=3;
+		// wait_x_cpu_clocks2(10000);
+	}
+}
+
 
 void	custom_delay(int milli)
 {
@@ -18,10 +37,24 @@ void	custom_delay(int milli)
 int		main()
 {
 	DDRB |= (1 << PIN3);
-	while (true)
+	// DDRB |= (1 << PIN2);
+	PORTB &= (0 << PIN3);
+	// PORTB |= (1 << PIN2);
+
+	// PORTB = 0b00001100;
+
+	for(;;)
 	{
-		PORTB ^= (1 << PIN3);
-		custom_delay(500);
+		// int cpu_clocks = 10000;
+	 	// while (cpu_clocks > 0)
+		// {
+
+		// // NOP();
+		// 	cpu_clocks-=1;
+			wait_x_cpu_clocks(2000000);
+
+		// }
+	 	PORTB ^= (1 << PIN3);
 	}
 	return (0);
 }
